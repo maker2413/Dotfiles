96dc6eb0745797a55f40ef8c0e632a96ab4f3ddc
maker2413 <maker2413@gmail.com>
2021-01-01 18:32:07 -0800

    added terraform mode to emacs and updated awesome keybinds

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -293,8 +293,8 @@ globalkeys = gears.table.join(
               {description = "increase the number of columns", group = "layout"}),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
               {description = "decrease the number of columns", group = "layout"}),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
-              {description = "select next", group = "layout"}),
+    awful.key({ modkey,           }, "space", function() menubar.show() end,
+              {description = "show the menubar", group = "launcher"}),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
               {description = "select previous", group = "layout"}),
 
@@ -325,8 +325,8 @@ globalkeys = gears.table.join(
               end,
               {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
+    awful.key({ modkey }, "p", function () awful.layout.inc( 1)                end,
+              {description = "select next", group = "layout"})
 )
 
 clientkeys = gears.table.join(
