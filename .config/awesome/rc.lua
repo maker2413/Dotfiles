87e51cd2c482e52ab44372dd6dff7c67323b5d44
maker2413 <maker2413@gmail.com>
2021-04-07 19:13:03 -0700

    brought function back

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -39,20 +39,22 @@ beautiful.init(require('theme'))
 -- ░█▄█░█▀█░█░░░█░░░█▀▀░█▀█░█▀▀░█▀▀░█▀▄
 -- ░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░░░▀░▀░▀░░░▀▀▀░▀░▀
 
+local function set_wallpaper(s)
+    -- Wallpaper
+    if beautiful.wallpaper then
+        local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        if type(wallpaper) == "function" then
+            wallpaper = wallpaper(s)
+        end
+        gears.wallpaper.maximized(wallpaper, s, true)
+    end
+end
+
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal(
    "property::geometry",
-   function(s)
-      -- Wallpaper
-      if beautiful.wallpaper then
-         local wallpaper = beautiful.wallpaper
-         -- If wallpaper is a function, call it with the screen
-         if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-         end
-         gears.wallpaper.maximized(wallpaper, s, true)
-      end
-   end
+   set_wallpaper
 )
 
 -- -- If LuaRocks is installed, make sure that packages installed through it are
