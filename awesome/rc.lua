d3045bf366ccded293cb01d93051c0b184dc1af0
maker2413 <maker2413@gmail.com>
2021-06-03 15:10:33 -0700

    renamed awesome tags and added new alias

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -207,7 +207,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "WWW", "Chat", "Term", "Dev", "Game", "Music", "VM", "HTOP", "Steam" }, s, awful.layout.layouts[1])
+    awful.tag({ "WWW", "Dev", "Term", "Chat", "Game", "Music", "VM", "HTOP", "Steam" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
