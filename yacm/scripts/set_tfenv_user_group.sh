7a5997e423fd5347609ee3db12bd26c9c7acd308
maker2413 <maker2413@gmail.com>
2022-10-15 15:37:34 -0500

    fixed bugs in tfenv and fish yacm scripts

--- yacm/scripts/set_tfenv_user_group.sh
+++ yacm/scripts/set_tfenv_user_group.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-TFENV_USER=$1;
+TFENV_USER="$1";
 
-if [[ -z $TFENV_USER ]]; then
-  if [[ $(grep 'tfenv' /etc/group | grep $TFENV_USER) ]]; then
-    echo -e "User: $TFENV_USER is in tfenv group already";
+if [[ ! -z "$TFENV_USER" ]]; then
+  grep tfenv /etc/group | grep -q "$TFENV_USER";
+  if [[ "$?" != 0 ]]; then
+    echo -e "Adding \e[32m$TFENV_USER\e[0m to group tfenv...";
+    sudo usermod -aG tfenv "$TFENV_USER"
   else
-    sudo usermod -aG tfenv $TFENV_USER
+    echo -e "User \e[32m$TFENV_USER\e[0m is in tfenv group already";
   fi
+else
+  echo -e "\e[31mUser not defined\e[0m";
 fi
--- yacm/scripts/set_user_shell_fish.sh
+++ yacm/scripts/set_user_shell_fish.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-FISH_USER=$1;
+FISH_USER="$1";
 
-if [[ -z $FISH_SHELL ]]; then
-  if [[ $(cat /etc/passwd | grep $FISH_USER | awk -F: '{print $7}') != '/usr/bin/fish' ]]; then
-    chsh -s /usr/bin/fish $FISH_USER;
+if [[ ! -z "$FISH_USER" ]]; then
+  if [[ $(cat /etc/passwd | grep "$FISH_USER" | awk -F: '{print $7}') != '/usr/bin/fish' ]]; then
+    chsh -s /usr/bin/fish "$FISH_USER";
   else
-    echo -e "User: $FISH_USER already using fish shell";
+    echo -e "User \e[32m$FISH_USER\e[0m already using fish shell";
   fi
+else
+  echo "User not defined";
 fi
