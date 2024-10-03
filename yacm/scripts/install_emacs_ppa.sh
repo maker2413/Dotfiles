fdd97afd679411352a2e3442e584c051ba3ce670
maker2413 <maker2413@gmail.com>
2024-10-03 00:21:47 -0500

    configured elementary os via yacm

---
+++ yacm/scripts/install_emacs_ppa.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e;
+
+sudo apt --yes install software-properties-common
+
+sudo add-apt-repository ppa:ubuntuhandbook1/emacs -y
+
+sudo DEBIAN_FRONTEND=noninteractive apt --yes install emacs
